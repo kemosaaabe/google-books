b75b755ecc9ef98243df2d8581a9f2729bcf07ff
kemosaaabe <sanchellos27@mail.ru>
2023-03-23 15:10:08 +0300

    [+] simple HTML header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,7 +1,30 @@
 import styles from './Header.module.scss';
 
 const Header = () => {
-    return <header className={styles.header}>всем привет</header>;
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerInner}>
+                <h1>Search For Books</h1>
+                <div className={styles.findField}>
+                    <input type="text" />
+                </div>
+                <div className={styles.filters}>
+                    <div className={styles.categories}>
+                        <label htmlFor="bookCategories">Categories</label>
+                        <select id="bookCategories">
+                            <option value="all">all</option>
+                        </select>
+                    </div>
+                    <div className={styles.sorting}>
+                        <label htmlFor="bookSorting">Sorting by</label>
+                        <select id="bookSorting">
+                            <option value="relevance">relevance </option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
