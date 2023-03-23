0e02eb9e98213ca5becb7107e9f01dc09aaf3f61
kemosaaabe <sanchellos27@mail.ru>
2023-03-23 14:52:08 +0300

    [~] init refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from './layout/Header';
 
 function App() {
-    return <div className="App">Hello</div>;
+    return <Header />;
 }
 
 export default App;
---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return <header className={styles.header}>всем привет</header>;
+};
+
+export default Header;
