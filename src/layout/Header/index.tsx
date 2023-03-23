8a123bef851d4d2e12f35424540e7afd85cabee7
kemosaaabe <sanchellos27@mail.ru>
2023-03-23 16:43:19 +0300

    [+] created env file [+] test axios on google api

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,13 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+
 import styles from './Header.module.scss';
 
 const Header = () => {
+    const { REACT_APP_API_KEY } = process.env;
+
+    const [findValue, setFindValue] = React.useState('');
+    const onFind = async () => {
+        try {
+            const { data } = await axios.get(
+                `https://www.googleapis.com/books/v1/volumes?q=${findValue}&key=${REACT_APP_API_KEY}`
+            );
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.headerInner}>
                 <h1>Search for Books</h1>
                 <div className={styles.findField}>
-                    <input type="text" />
-                    <img src="/assets/img/icons/search.svg" alt="search" />
+                    <input
+                        type="text"
+                        value={findValue}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                            setFindValue(e.currentTarget.value);
+                        }}
+                    />
+                    <img
+                        src="/assets/img/icons/search.svg"
+                        alt="search"
+                        onClick={onFind}
+                    />
                 </div>
                 <div className={styles.filters}>
                     <div className={styles.categories}>
